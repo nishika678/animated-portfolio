53ee9fdac6a05d659dcc7fd6c85d652f4232b0b4
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-03 00:47:54 +0530

    Update Test.jsx

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,13 +1,23 @@
 import {motion} from 'framer-motion'
+import { useState } from 'react'
 
 const Test=()=> {
+
+  const [open,setOpen]=useState(false)
+
+  const variants={
+   visible:{opacity:1,x:800,transition:{type:"spring",stiffness:100,damping:100}},
+   hidden:{opacity:0}, 
+  }
+
+  const items=["item1","item2","item3","item4"];
   return (
     <div className='course'>
-        <motion.div className="box" 
-        initial={{opacity:0.5,scale:0.5}}
-        transition={{duration:2}}
-        whileHover={{opacity:1,scale:2}}>
-        </motion.div>
+     <motion.ul>
+      {items.map((item)=>(
+      <motion.li></motion.li>
+      ))}
+      </motion.ul> 
     </div>
   )
 }
