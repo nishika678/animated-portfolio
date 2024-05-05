85566c6dec8f1d7100e37ae83542a77436799ebf
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-05 15:15:55 +0530

    Update Test.jsx

--- src/Test.jsx
+++ src/Test.jsx
@@ -6,20 +6,24 @@ const Test=()=> {
   const [open,setOpen]=useState(false)
 
   const variants={
-   visible:{opacity:1,x:800,transition:{type:"spring",stiffness:100,damping:100}},
+   visible:(i)=>({opacity:1,
+    x:800,
+    transition:{ delay:i*0.3},
+  }),
    hidden:{opacity:0}, 
-  }
+  };
 
   const items=["item1","item2","item3","item4"];
+
   return (
     <div className='course'>
-     <motion.ul>
-      {items.map((item)=>(
-      <motion.li></motion.li>
+     <motion.ul initial="hidden" animate="visible" variants={variants}>
+      {items.map((item,i)=>(
+      <motion.li variants={variants} key={item}custom={i}>{item}</motion.li>
       ))}
       </motion.ul> 
     </div>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
