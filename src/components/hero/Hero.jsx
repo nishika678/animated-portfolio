b65453b7cbb02adc533267b79af3f4cbb4871beb
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-09 00:23:14 +0530

    Create Hero.jsx

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,23 @@
+import "./hero.scss"
+
+function Hero() {
+  return (
+    <div className="hero">
+        <div className="textContainer">
+            <h2>HARVEY TYLER</h2>
+            <h1>Web developer and UI designer</h1>
+            <div className="buttons">
+            <button>See the Latest Work</button>
+            <button>Contact Me</button>
+            </div>
+            <img src="/scroll.png" alt="" />
+        </div>
+
+        <div className="imageContainer">
+            <img src="/hero.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
