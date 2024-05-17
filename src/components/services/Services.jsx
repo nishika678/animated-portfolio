6d11df371cab77bffc49d848055c631f4c6b4c1d
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-17 23:58:10 +0530

    Create Services.jsx

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,25 @@
+import "./services.scss"
+import {motion} from "framer-motion"
+const Services=()=> {
+  return (
+    <motion.div className="services">
+        <motion.div className="textContainer">
+            <p>I focus on helping your brand grow <br /> and move forward</p>
+            <hr />
+        </motion.div>
+        <motion.div className="titleContainer">
+            <div className="title">
+                <img src="/people.webp" alt="" />
+                <h1><b>Unique</b> Ideas</h1>
+            </div>
+            <div className="title">
+                <h1><b>For Your</b> Buisness.</h1>
+                <button>WHAT WE DO?</button>
+            </div>
+        </motion.div>
+        <motion.div className="listContainer"></motion.div>
+    </motion.div>
+  )
+}
+
+export default Services
