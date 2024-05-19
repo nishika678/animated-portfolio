7f45651b73b8d60567c746be3aff9b1064d94ec4
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-19 23:16:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 const App = () => {
   return <div>
     <section id="Homepage">
@@ -9,11 +11,9 @@ const App = () => {
       <Hero />
     </section>
     <section id="Services"><Parallax type="services"/></section>
-    <section>Services</section>
+    <section><Services /></section>
     <section id="Portfolio"><Parallax type="portfolio"/></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <Portfolio />
     <section id="Contact">Contact</section> 
   </div>;
 };
